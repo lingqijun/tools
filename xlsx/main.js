1ee920c2b9023d5a37b1e852b508a225d356a15d
lingqijun <lingqijun@whalejoy.com>
2019-05-08 18:02:48 +0800

    优化xlsx工具功能

--- xlsx/main.js
+++ xlsx/main.js
@@ -21,18 +21,78 @@ fs.readdirSync(__dirname + '/config/').filter(function (file) {
 }).forEach(function (file) {
 	const path = __dirname + '/config/' + file;
 	const sheets = xlsx.parse(fs.readFileSync(path)) || [];
-	const name = file.split('.')[0];
-	console.log('Name = ', name);
-	if (sheets.length === 1) {
-		let sheet = sheets[0].data;
+	const fileName = file.split('.')[0];
 
-		let data = {};
+	if (!sheets.length){
+		console.log('xlsx file context is null');
+		return ;
+	}
 
+	console.log('fileName = ', fileName);
+
+	for (let sheet of sheets){
+		let sheetData = sheet.data;
+
+		if (!sheetData.length){
+			console.log(sheet.name +' context is null');
+			return ;
+		}
+
+		let classInfo = sheetData[0];
+
+		let data = null;
+		let name = null;
+		let className = null;
+		let jsonName = null;
+
+		let classType = null;
+		for (let i = 0; i < classInfo.length; i++) {
+			let context = classInfo[i];
+			if (!context || context.indexOf("=") === 0) {
+				break
+			}
+			let contextTable = context.split('=');
+			let field = contextTable[0];
+			let value = contextTable[1];
+
+			if (field === 'class'){
+				classType = value;
+			}
+
+			if (field === 'name'){
+				name = value;
+			}
+
+			if (field === 'className'){
+				className = value;
+			}
+
+			if (field === 'jsonName'){
+				jsonName = value;
+			}
+		}
+
+		if (classType === 'array' || classType === 'Array' ){
+			data = [];
+		}
+		else if (classType === 'object' || classType === 'Object' ){
+			data = {};
+		}
+
+		if (!data){
+			return console.log('class type is null');
+		}
+
+		name = name || fileName;
+		className = className || name;
+		jsonName = jsonName || name;
+
+		let index = 0;
 		let dateTypeArray = null;
+		for (let i = 0; i < sheetData.length; i++) {
+			let lines = sheetData[i];
+			let info = null;
 
-		for (let i = 0; i < sheet.length; i++) {
-			let lines = sheet[i];
-			let info = {};
 			for (let j = 0; j < lines.length; j++) {
 				let context = lines[j];
 				if (context === undefined || context === null || context[0] === '#') {
@@ -51,9 +111,22 @@ fs.readdirSync(__dirname + '/config/').filter(function (file) {
 				let key = contextTable.key;
 				let type = contextTable.type;
 
-				if (type === 'kn') {
+				if (!info){
+					info = info || {};
+					if (classType === 'array' || classType === 'Array') {
+						info['index'] = index;
+						data.push(info);
+						index++;
+					}
+				}
+
+				if (type === 'ks' || type === 'k'){
 					data[context] = info;
-					info[key] = context;
+					info[key] = String(context);
+				}
+				else if (type === 'kn') {
+					data[context] = info;
+					info[key] = Number(context);
 				}
 
 				if (type === 's') {
@@ -70,9 +143,15 @@ fs.readdirSync(__dirname + '/config/').filter(function (file) {
 					info[key] = JSON.parse(context);
 				}
 
+				if (type === 'a'){
+
+				}
+
 			}
 
-			if (i === 1) {
+
+
+			if (i === 2) {
 				dateTypeArray = [];
 				for (let j = 0; j < lines.length; j++) {
 					let context = lines[j];
@@ -86,16 +165,20 @@ fs.readdirSync(__dirname + '/config/').filter(function (file) {
 					dateTypeArray.push({key: key, type: type});
 				}
 			}
+
+
 		}
-		config[name] = data;
 
-		fileContext += 'root.' + name + ' = ' + JSON.stringify(data, null, 5) + ';\n\t';
+		config[className] = data;
 
+		let jsonString = JSON.stringify(data, null, 4);
+
+		fileContext += 'root.' + className + ' = ' + jsonString + ';\n\t';
+
+		fs.writeFile('./models/' + jsonName + '.json', jsonString, function (err) {
+			console.log('err = ', err);
+		});
 	}
-	else if (sheets.length > 1) {
-
-	}
-
 
 });
 
@@ -104,3 +187,5 @@ fileContext += end;
 fs.writeFile('./models/config.js', fileContext, function (err) {
 	console.log('err = ', err);
 });
+
+
